2b7d9c78e525b6ddc8064cadc0a1da2e7297c076
Jose Yustiz <jose.yustiz@gmail.com>
2020-07-14 07:02:26 -0400

    changed title

--- src/frontend/palindrome-search/src/app/app.component.ts
+++ src/frontend/palindrome-search/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'palindrome-search';
+  title = 'Palindrome Search Challenge';
 }
