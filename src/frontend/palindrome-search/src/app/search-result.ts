b1e702b73f71a611d076983aed4db9cfadc716d2
Jose Yustiz <jose.yustiz@gmail.com>
2020-07-15 03:11:31 -0400

    search product response interfaces

---
+++ src/frontend/palindrome-search/src/app/search-result.ts
@@ -0,0 +1,17 @@
+import { Product } from './product';
+import { Sort } from './sort';
+import { Pageable } from './pageable';
+
+export interface SearchResult {
+    content: Product[];
+    pageable: Pageable;
+    last: boolean;
+    totalPages: number;
+    totalElements: number;
+    numberOfElements: number;
+    first: boolean;
+    sort: Sort;
+    size: number;
+    number: number;
+    empty: boolean;
+  }
