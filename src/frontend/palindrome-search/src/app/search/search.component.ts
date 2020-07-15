d0e4948de3e91b319b011265214dcb7ef349100f
Jose Yustiz <jose.yustiz@gmail.com>
2020-07-15 03:13:31 -0400

    basic search components

--- src/frontend/palindrome-search/src/app/search/search.component.ts
+++ src/frontend/palindrome-search/src/app/search/search.component.ts
@@ -1,18 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 import {FormGroup, FormBuilder, Validator, Validators} from '@angular/forms'
-
+import {SearchResult} from '../search-result'
+import {SearchService} from '../search.service'
+import {Search} from '../search'
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-  
+
   searchForm: FormGroup;
   searchError: boolean = false;
-  
-  constructor(private fb: FormBuilder) { }
+  searchResult: SearchResult = {
+    content: [],
+    pageable: {
+      sort: {
+        unsorted: false,
+        sorted: false,
+        empty: true
+      },
+      pageNumber: 0,
+      pageSize: 0,
+      offset: 0,
+      unpaged: false,
+      paged: false
+    },
+    totalPages: 1,
+    totalElements: 0,
+    last: true,
+    numberOfElements: 0,
+    first: true,
+    sort: {
+      unsorted: false,
+      sorted: false,
+      empty: true
+    },
+    size: 0,
+    number: 0,
+    empty: true
+  };
 
+  constructor(private fb: FormBuilder, private service: SearchService) { }
   ngOnInit(): void {
     this.createSearchForm();
   }
@@ -22,8 +51,17 @@ export class SearchComponent implements OnInit {
       phrase: ['', Validators.required]
     });
   }
-  
+
   submitSearchForm(){
     console.log("Form submited!")
+    const search: Search = {
+      phrase: this.searchForm.get("phrase").value,
+      page: 0,
+      size: 20
+    };
+
+    this.service.searchProduct(search)
+    .subscribe(result => this.searchResult = result);
+
   }
 }
